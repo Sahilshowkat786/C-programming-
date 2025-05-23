68fb258e32ec33d1039a7d89f2e14faf3ae77301
Sahilshowkat786 <sahilshowkatsofi@gmail.com>
2025-05-24 00:32:16 +0530

    Add files via upload

---
+++ chapter04(patterns)/11_floyds_triangle.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+// 1
+// 23
+// 456
+// 78910
+  
+
+int main (){
+int n;
+printf("Enter no of rows=");
+scanf("%d",&n);
+int a=1;
+for(int i=1;i<=n;i++){
+    for(int j =1;j<=i;j++){
+        
+    printf("%d ",a);
+a++;
+}
+
+printf("\n");
+}
+
+
+
+    return 0;
+}
